0cec3fa852d1028d2ad95fd5cbd19a934d6cf591
Larry Eby and Sara Frandsen <sara@>
2017-01-25 11:22:15 -0800

    Add button disable functionality and add various refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,10 @@ Pig.prototype.mudRoll = function() {
 
 function oneCheck(input, Pig) {
   if(input === 1) {
-    Pig.turnTotal = 0;
+    thePigs.forEach(function(item){
+      item.turnTotal = 0;
+    });
     thePigs.reverse();
-    thePigs[0].turnTotal = 0;
   } else {
     Pig.turnTotal += input;
   };
@@ -26,27 +27,49 @@ function winCheck(input){
     return true;
   } else {
     return false;
-  }
-}
+  };
+};
 
 //front-end
 $(function(){
   //Submit Listener
   $("#begin").submit(function(event) {
     event.preventDefault();
-
+    //Main Variables
     var name1 = $("#player1-name").val();
     var name2 = $("#player2-name").val();
     var newPig1 = new Pig(name1);
     var newPig2 = new Pig(name2);
     thePigs.push(newPig1, newPig2);
-    $(".winner-display").text(thePigs[0].pigName + "'s turn!")
 
+    // Front End Functions
+
+    var reset = function(){
+      thePigs.forEach(function(item){
+        item.gameTotal = 0;
+        item.turnTotal = 0;
+        $("#play-again").show();
+        $("#roll, #hold").addClass("disabled").prop('disabled', true);;
+      });
+    };
+
+    var showGameTotal = function(){
+      $(".player1-game-total").text(newPig1.gameTotal);
+      $(".player2-game-total").text(newPig2.gameTotal);
+    };
+
+    var whoseTurn = function() {
+      $(".winner-display").text(thePigs[0].pigName + "'s turn!")
+    };
+
+    //Game begin
 
     $(".start").hide();
     $("#first-pig").text(newPig1.pigName);
     $("#second-pig").text(newPig2.pigName);
     $(".game-pane").show();
+    whoseTurn();
+    showGameTotal();
 
     //Roll Listener
     $("#roll").click(function(){
@@ -54,27 +77,33 @@ $(function(){
       oneCheck(turnRoll, thePigs[0]);
       $(".turn-roll").text(turnRoll);
       $(".player-total").text(thePigs[0].turnTotal);
-      $(".winner-display").text(thePigs[0].pigName + "'s turn!")
+      $("#hold").removeClass("disabled").prop('disabled', false);
 
 
       $("#hold").off().click(function(){
         thePigs[0].gameTotal += thePigs[0].turnTotal;
+        $("#hold").addClass("disabled").prop('disabled', true);;
 
         if (winCheck(thePigs[0].gameTotal) === true) {
           $(".winner-display").text(thePigs[0].pigName + " wins with " + thePigs[0].gameTotal + " points!");
-          thePigs.forEach(function(item){
-            item.gameTotal = 0;
-            item.turnTotal = 0;
-          });
+          reset();
+          showGameTotal();
         } else {
           thePigs.reverse();
-          $(".winner-display").text(thePigs[0].pigName + "'s turn!")
+          whoseTurn();
+          showGameTotal();
           thePigs[0].turnTotal = 0;
         };
         $(".turn-roll").text(turnRoll);
         $(".player-total").text(thePigs[0].turnTotal);
-        $(".player1-game-total").text(newPig1.gameTotal);
-        $(".player2-game-total").text(newPig2.gameTotal);
+      });
+
+      $("#play-again").off().click(function(){
+        reset();
+        whoseTurn();
+        showGameTotal();
+        $("#play-again").hide();
+        $("#roll, #hold").removeClass("disabled").prop('disabled', false);;
       });
     });
   });
